70409ed14990cb496d498278a5106324753698cd
EvertonMirand <everton.mirandav@gmail.com>
2020-10-01 21:17:56 -0300

    Client Side Fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+
+import useSWR from "swr";
+import { useFetch } from "../services/useFetch";
+
 import { Title } from "../styles/pages/Home";
 
+interface IProduct {
+  id: string;
+  title: string;
+}
+
 export default function Home() {
+  const { data: recommendedProducts } = useFetch<IProduct[]>("recommended");
+
+  if (!recommendedProducts) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div>
-      <Title>Oi</Title>
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map(({ id, title }) => (
+            <li key={id}>{title}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
